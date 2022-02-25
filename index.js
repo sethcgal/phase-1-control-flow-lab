b7f1e154efd681765b30578697193fca97b8d3ad
Seth <stormtroopers2003@gmail.com>
2022-02-25 11:06:09 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let response
+    if (distance <= 400) {
+      response = "This one is on me!"
+      return response;
+    } else if (distance <= 2499 && distance >=2000){
+      response = "I will gladly take your thirty bucks."
+      return response;
+    } else if (distance >=2500){
+      response = "No can do."
+      return response;}
+    }
+function ternaryCheckCity(city){
+  return (city == 'NYC') ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+      case 'not as generous':
+        return 'Thank you.';
+        break;
+        case 'thanks for everything':
+          return 'Bye.';
+          break;
+  }
+  
 }
